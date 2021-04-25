cbb456e8393d00af96054129342309371a89050e
Helge Heß <devteam@zeezide.de>
2021-04-25 16:48:27 +0200

    Only import Foundation if available
    ... i.e. necessary.

--- Sources/Mustache/Helpers.swift
+++ Sources/Mustache/Helpers.swift
@@ -3,27 +3,37 @@
 //  Noze.io
 //
 //  Created by Helge Heß on 6/1/16.
-//  Copyright © 2016 ZeeZide GmbH. All rights reserved.
+//  Copyright © 2016-2021 ZeeZide GmbH. All rights reserved.
 //
 
-import Foundation
+#if canImport(Foundation)
+import class Foundation.NSNumber
+import class Foundation.NSString
+import class Foundation.NSValue
+#endif
 
 public extension MustacheRenderingContext {
 
+  @inlinable
   func isFoundationBaseType(value vv: Any) -> Bool {
+    #if canImport(Foundation)
     if vv is NSNumber { return true }
     if vv is NSString { return true }
     if vv is NSValue  { return true }
+    #endif
     return false
   }
 
+  @inlinable
   func isMustacheTrue(value v: Any?) -> Bool {
     guard let vv = v else { return false }
     
     if let b = vv as? Bool     { return b }
     if let i = vv as? Int      { return i == 0 ? false : true }
     if let s = vv as? String   { return !s.isEmpty }
+    #if canImport(Foundation)
     if let n = vv as? NSNumber { return n.boolValue }
+    #endif
     
     let mirror = Mirror(reflecting: vv)
     
@@ -34,5 +44,4 @@ public extension MustacheRenderingContext {
     guard ds == .collection else { return true } // all objects
     return mirror.children.count > 0
   }
-
 }
